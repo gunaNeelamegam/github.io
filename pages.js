fbae841af1e0f495aa55977c2013c368e935ce98
gunaNeelamegam <=gunag77730@gmail.com>
2022-09-20 17:42:01 +0530

    portfolio

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/gunaNeelamegam/guna.io.git";
+const repoURL = "https://github.com/gunaNeelamegam/Guna.github.io.git";
 //github.com/gunaNeelamegam/home.git
 https: ghpages.publish(
   pathname,
